c344be746473cc2f636deefefa6078cdbeab3794
xiphiasphotography <mw.rosa@gmail.com>
2020-02-22 17:48:56 +0100

    Reordering footer items.

--- plog-content/themes/xip-bs/footer.php
+++ plog-content/themes/xip-bs/footer.php
@@ -1,7 +1,7 @@
 	<footer id="footer" class="container-fluid">
 
 		<div class="row gutters-10">
-			<div class="col-12 order-1 col-md-6 order-md-2 col-lg-12 order-lg-1">
+			<div class="col-12 order-1 col-md-6 col-lg-12 order-lg-1">
 				<?php if (plogger_pagination_control() != '') { ?>
 					<div id="pagination">
 						<?php echo plogger_pagination_control(5); ?>
@@ -9,7 +9,7 @@
 				<?php } ?>
 			</div>
 
-			<div class="col-12 order-2 col-md-6 order-md-1 col-lg-4 order-lg-4">
+			<div class="col-12 order-2 col-md-6 col-lg-4 order-lg-4">
 				<?php if (plogger_sort_control() != '') { ?>
 					<div id="sort-control">
 						<?php echo plogger_sort_control(); ?>
@@ -17,22 +17,21 @@
 				<?php } ?>
 			</div>
 
-			<div class="col-12 order-3 col-md-6 col-sm-push-6 col-lg-4 order-lg-2">
+			<div class="col-12 order-3 col-md-6 col-lg-4 order-lg-2">
 				<?php if (generate_jump_menu() != '') { ?>
 					<div id="navigation-container">
 						<?php echo generate_jump_menu(); ?>
 					</div><!-- /navigation-container -->
 				<?php } ?>
 			</div>
-			<div class="col-12 order-4 col-md-6 col-sm-pull-6 col-lg-4 order-lg-3">
+
+			<div class="col-12 order-4 col-md-6 col-lg-4 order-lg-3">
 				<div id="search-container">
 					<?php echo generate_search_box(); ?>
 				</div><!-- /search-container -->
 			</div>
-		</div><!-- /row -->
 
-		<div class="row gutters-10">
-			<div class="col-12">
+			<div class="col-12 order-5 col-md-6 col-lg-4">
 				<?php if (plogger_download_selected_button() != '') { ?>
 					<div id="download-selected"><?php echo plogger_download_selected_button(); ?></div><!-- /download-selected -->
 				<?php } ?>
@@ -41,13 +40,13 @@
 
 		<div class="row gutters-10">
 			<div class="col-10">
-				<span class="link-back"><?php echo plogger_link_back(); ?></span>
+				<span class="link-back"><?php echo plogger_link_back(); ?>.</span>
 				<span class="credit">
 					<a href="http://xiphias.photography/" title="xiphias.photography"><?php echo plog_tr('Design by') ?> xiphias.photography</a>
 				</span><!-- /credit -->
 			</div>
 
-			<div class="col-2">
+			<div class="col-2 text-right">
 				<?php if (plogger_rss_feed_button() != '') { ?>
 					<div id="rss-tag-container"><?php echo plogger_rss_feed_button(); ?></div><!-- /rss-tag-container -->
 				<?php } ?>
