af70fa6969df3d7abf23d11f5ef195590c469385
xiphiasphotography <mw.rosa@gmail.com>
2020-02-22 11:43:16 +0100

    Changed back to <?= GALLERY_URL ?>

--- plog-content/themes/xip-bs/head.php
+++ plog-content/themes/xip-bs/head.php
@@ -6,10 +6,10 @@
 {
   "@context": "http://schema.org",
   "@type": "WebSite",
-  "url": "<?php echo $config['gallery_url']; ?>",
+  "url": "<?= GALLERY_URL ?>",
   "potentialAction": {
     "@type": "SearchAction",
-    "target": "<?php echo $config['gallery_url']; ?>?searchterms={search_term_string}&level=search",
+    "target": "<?= GALLERY_URL ?>?searchterms={search_term_string}&level=search",
     "query-input": "required name=search_term_string"
   }
 }
