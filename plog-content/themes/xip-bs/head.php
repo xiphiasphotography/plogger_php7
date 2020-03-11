79f7a8f97ff5a151201b0e225ed636536f515035
Michael Rosa <mw.rosa@gmail.com>
2020-03-11 12:41:17 +0100

    Added favicon

--- plog-content/themes/xip-bs/head.php
+++ plog-content/themes/xip-bs/head.php
@@ -2,11 +2,16 @@
 
 <?php echo plogger_generate_seo_meta_tags(); ?>
 
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+<link rel="icon" href="favicon.ico" type="image/x-icon">
+
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Montserrat|Oswald&display=swap" />
 <link rel="stylesheet" type="text/css" href="<?= THEME_URL ?>jscss/fontawesome.min.css" media="screen" />
 <link rel="stylesheet" type="text/css" href="<?= THEME_URL ?>jscss/gallery.css" media="screen" />
+
 <script type="text/javascript" src="<?= THEME_URL ?>jscss/dynamics.js"></script>
+
 <script type="application/ld+json">
 {
   "@context": "http://schema.org",
